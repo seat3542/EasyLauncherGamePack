09a9cf76c889617b872eac992dd27e081838e7ca
Alex Shinsel <shin2448@linux-1uih.site>
2014-11-22 20:56:49 -0800

    Finished hangman

--- Hangman/include/hangman.h
+++ Hangman/include/hangman.h
@@ -1,26 +1,32 @@
-#ifndef HANGMAN_H_
-#define HANGMAN_H_
+/****************************************************************************
+ * File: 				hangman.h
+ * Author:			littlefiredragon @ github
+ * Description: A text-based hangman game made for the Easy Launcher
+ * 							Game Pack.
+ ***************************************************************************/
+#ifndef HANGMAN_H
+#define HANGMAN_H
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct Alphabet
 {
 	char letter;
-	bool bInWord;
 	bool bGuessed;
+	bool bInWord;
 } Alphabet;
 
 typedef struct Hangman
 {
+	char pictures[8][37];
+	Alphabet sAlphabet[26];
 	char *word;
 	int wordLength;
-	Alphabet sAlphabet[26];
 	int hangingMan;
-	char pictures[8][37];
 } Hangman;
 
 void initializeAlphabet (Alphabet sAlphabet[], char *word, int wordlength);
@@ -32,4 +38,4 @@ void takeTurn (Hangman *psGame);
 bool gameIsOver (Hangman *psGame);
 bool gameIsWon (Hangman *psGame);
 
-#endif /* HANGMAN_H_ */
+#endif /* HANGMAN_H */
